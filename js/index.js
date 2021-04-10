864d26f3782baa31a5c7c0b452ee93225433c21c
Carlos Herrera <carlosger_64@hotmail.com>
2021-04-10 18:53:21 -0500

    js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 var gl; //contexto WebGL
+start();
 
 function start() {
   var canvas = document.getElementById("glcanvas");
@@ -11,3 +12,24 @@ function start() {
     gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);      // Limpiar el buffer de color asi como el de profundidad
   }
 }
+
+function initWebGL(canvas) {
+    gl = null;
+  
+    try {
+      // Tratar de tomar el contexto estandar. Si falla, retornar al experimental.
+      gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    }
+    catch(e) {}
+  
+    if (!gl) {
+      alert("Tu navegador no soporta WebGL.");
+      gl = null;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  
+    return gl;
+  }
+  
