f7a59098659e215dfbed2bbea4a221f143e9a548
Carlos Herrera <carlosger_64@hotmail.com>
2021-04-10 19:01:43 -0500

    webgl

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function start() {
     gl.clearColor(1.0, 1.0, 0.0, 0.5);                      // Establecer el color base en negro, totalmente opaco
     gl.enable(gl.DEPTH_TEST);                               // Habilitar prueba de profundidad
     gl.depthFunc(gl.LEQUAL);                                // Objetos cercanos opacan objetos lejanos
-    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);      // Limpiar el buffer de color asi como el de profundidad
+    //gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);      // Limpiar el buffer de color asi como el de profundidad
   }
 }
 
